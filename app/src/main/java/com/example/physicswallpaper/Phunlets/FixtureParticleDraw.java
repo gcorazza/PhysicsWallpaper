6b909ef6d8cb47ab62205aeaef76e0cb56650d7c
Gianluca Corazza <gianluca.corazza@klosebrothers.de>
2019-12-05 22:44:40 +0100

    added particle Draw

--- app/src/main/java/com/example/physicswallpaper/Phunlets/FixtureParticleDraw.java
+++ app/src/main/java/com/example/physicswallpaper/Phunlets/FixtureParticleDraw.java
@@ -1,9 +1,12 @@
 package com.example.physicswallpaper.Phunlets;
 
 import android.graphics.Canvas;
+import android.graphics.RectF;
 
 import org.jbox2d.common.Transform;
 import org.jbox2d.dynamics.Fixture;
+
+import static java.lang.Math.toDegrees;
 
 public class FixtureParticleDraw extends FixtureDraw {
 
@@ -11,8 +14,23 @@ public class FixtureParticleDraw extends FixtureDraw {
         super(color, fixture);
     }
 
+    private long startTime = System.currentTimeMillis();
+    private int timeToLiveMs = 1000;
+
     @Override
     public void draw(Canvas canvas, Transform transform) {
+        canvas.save();
+        canvas.translate(transform.p.x, transform.p.y);
+        canvas.rotate((float) (toDegrees(transform.q.getAngle())));
+        canvas.translate(offset.x, offset.y);
+        canvas.rotate((float) toDegrees(offAngle));
+        float pw = 0.1f;
+        innerPaint.setAlpha(255 - alpha());
+        canvas.drawRect(new RectF(-pw, -pw, pw, pw), innerPaint);
+        canvas.restore();
+    }
 
+    private int alpha() {
+        return (int) (((float) (System.currentTimeMillis() - startTime)) / timeToLiveMs * 255);
     }
 }
