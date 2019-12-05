2c9719cede161e4bcbbbf3d103d62d8a6ab1ca82
Gianluca Corazza <gianluca.corazza@klosebrothers.de>
2019-12-05 22:27:04 +0100

    added cicle Draw

--- app/src/main/java/com/example/physicswallpaper/Phunlets/FixtureDrawCircle.java
+++ app/src/main/java/com/example/physicswallpaper/Phunlets/FixtureDrawCircle.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import org.jbox2d.common.Transform;
 import org.jbox2d.dynamics.Fixture;
 
+import static java.lang.Math.toDegrees;
+
 public class FixtureDrawCircle extends FixtureDraw {
 
     private float radius;
@@ -16,7 +18,15 @@ public class FixtureDrawCircle extends FixtureDraw {
 
     @Override
     public void draw(Canvas canvas, Transform transform) {
-        //canvas.drawArc();
+        canvas.save();
+        canvas.translate(transform.p.x, transform.p.y);
+        canvas.rotate((float) (toDegrees(transform.q.getAngle())));
+        canvas.translate(offset.x, offset.y);
+        canvas.rotate((float) toDegrees(offAngle));
+        canvas.drawCircle(0, 0, radius, innerPaint);
+        canvas.drawCircle(0, 0, radius, outerPaint);
+        canvas.drawLine(0, 0, radius, 0, outerPaint);
+        canvas.restore();
     }
 
     public float getRadius() {
